c57861310f8adc3eb0b93a9da663561b2532a88c
wk-001 <2326699650@qq.com>
2019-10-24 17:28:07 +0800

    导航栏添加修改密码功能

--- spring-boot-4-shiro/src/main/java/com/wk/controller/UserController.java
+++ spring-boot-4-shiro/src/main/java/com/wk/controller/UserController.java
@@ -1,11 +1,19 @@
 package com.wk.controller;
 
 
+import com.wk.pojo.User;
+import com.wk.service.UserService;
+import com.wk.shiro.ShiroUtil;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.session.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
@@ -18,6 +26,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+	@Autowired
+	private UserService userService;
 
 	//该用户具有"user:list"的权限才能执行此方法
 	@RequiresPermissions("user:list")
@@ -41,4 +52,25 @@ public class UserController {
 		model.addAttribute("msg","删除用户信息");
 		return "user/list";
 	}
+
+	@RequestMapping("checkPassword")
+	@ResponseBody
+	public int checkPassword(String oldPwd,String newPwd){
+		Session session = SecurityUtils.getSubject().getSession();
+		User user = (User) session.getAttribute("user");
+		Integer id = user.getId();
+		String salt = user.getSalt();
+		int times = 2;
+		String encodedPassword = new SimpleHash("MD5", oldPwd, salt, times).toString();
+		User userByDb = userService.getById(id);
+		if(userByDb.getPassword().equals(encodedPassword)){
+			user.setPassword(newPwd);
+			User user1 = ShiroUtil.encryptPassword(user);
+			userService.updateById(user1);
+			return 1;
+		}else {
+			return 2;
+		}
+	}
+
 }
