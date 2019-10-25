e6df7646eca138323c5db8ed03a96de6301b02e1
wk-001 <2326699650@qq.com>
2019-10-25 16:51:31 +0800

    优化页面显示

--- spring-boot-4-shiro/src/main/java/com/wk/controller/UserController.java
+++ spring-boot-4-shiro/src/main/java/com/wk/controller/UserController.java
@@ -5,13 +5,10 @@ import com.wk.pojo.User;
 import com.wk.service.UserService;
 import com.wk.shiro.ShiroUtil;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.session.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -29,29 +26,6 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-
-	//该用户具有"user:list"的权限才能执行此方法
-	@RequiresPermissions("user:list")
-	@RequestMapping("list")
-	public String userList(Model model){
-		model.addAttribute("msg","获取用户信息");
-		return "user/list";
-	}
-
-	@RequiresPermissions("user:update")
-	@RequestMapping("update")
-	public String userUpdate(Model model){
-		model.addAttribute("msg","修改用户信息");
-		return "user/list";
-	}
-
-	//该用户具有"admin"的角色才能执行此方法
-	@RequiresRoles("admin")
-	@RequestMapping("del")
-	public String userDel(Model model){
-		model.addAttribute("msg","删除用户信息");
-		return "user/list";
-	}
 
 	@RequestMapping("checkPassword")
 	@ResponseBody
