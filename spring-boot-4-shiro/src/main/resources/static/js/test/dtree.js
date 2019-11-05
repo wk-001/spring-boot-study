68d882677279e1f5d27d35c647a67dbfea3575a7
wk <123@qq.com>
2019-11-05 22:29:20 +0800

    layui 树形结构

--- spring-boot-4-shiro/src/main/resources/static/js/test/dtree.js
+++ spring-boot-4-shiro/src/main/resources/static/js/test/dtree.js
@@ -8,8 +8,27 @@ layui.extend({
     // 初始化树
     var DemoTree = dtree.render({
         elem: "#demoTree",  //页面容器
-        // url: '/json/asyncTree1.json' 使用url加载（可与data加载同时存在）
-        url:'/plugin/layui-v2.5.5/layui_ext/json/case/asyncTree2.json'
+        method:'get',           //访问本地json文件必须用get请求，默认是post请求！！！
+
+        /*layui+list集合数据格式（dataStyle + dataFormat）*/
+        url: "/plugin/layui-v2.5.5/layui_ext/json/case/styleTree13.json",
+        dataStyle: "layuiStyle",  //使用layui风格的数据格式
+        dataFormat: "list",  //配置data的风格为list
+        response:{message:"msg",statusCode:0},  //修改response中返回数据的定义
+
+        /*dataFormat:"list",      //使用list风格的数据格式,从数据库查出的数据可以直接使用
+        url:'/plugin/layui-v2.5.5/layui_ext/json/case/styleTree12.json',*/
+
+        /*dataStyle: "layuiStyle",  //使用layui风格的数据格式
+        url:'/plugin/layui-v2.5.5/layui_ext/json/case/asyncTree10.json',     //layui风格的数据格式
+        response:{message:"msg",statusCode:0},  //使用layui风格需要修改response中返回数据的定义*/
+
+        //url:'/plugin/layui-v2.5.5/layui_ext/json/case/asyncTree1.json',     //标准数据格式
+
+        initLevel: 1,
+        checkbar: true,
+        checkbarType: "all",
+        skin: "layui"
     });
 
     // 绑定节点点击
