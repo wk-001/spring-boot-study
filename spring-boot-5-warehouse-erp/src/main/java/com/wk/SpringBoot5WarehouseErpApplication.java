f7cdf570c3121262e958f23a168b569e27a2bda0
wk <123@qq.com>
2019-11-24 22:16:00 +0800

    仓库管理ERP系统环境搭建

---
+++ spring-boot-5-warehouse-erp/src/main/java/com/wk/SpringBoot5WarehouseErpApplication.java
@@ -0,0 +1,13 @@
+package com.wk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot5WarehouseErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot5WarehouseErpApplication.class, args);
+    }
+
+}
